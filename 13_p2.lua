5255117424a519f94e7408cf36e2e15af08b9809
vdshesap13-lgtm <vdshesap13@gmail.com>
2026-06-27 20:11:18 -0700

    Update 13_p2.lua

--- 13_p2.lua
+++ 13_p2.lua
@@ -1,6 +1,6 @@
 -- Ken HUB Part 2/5 - UI Shell
 -- Ken HUB Part 2/5 - UI Shell
-local SCRIPT_VERSION = "1.76"
+local SCRIPT_VERSION = "1.78"
 local K = _G.KenHubState
 if not K or not K.CONFIG then
     error("[Ken HUB] Part 1 yuklenmedi - Loader.lua kullan")
